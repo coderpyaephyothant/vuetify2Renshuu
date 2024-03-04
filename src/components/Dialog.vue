5a58b3c09b5621c90d9510db4c33b0e2a6799ac7
coderpyaephyothant <pyaephyothantengr520@gmail.com>
2024-03-04 15:33:53 +0630

    datePickerFormTest

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -27,15 +27,63 @@
             Create Project
         </v-card-title>
         <v-card-text>
-            This is Dialog
+            <v-form>
+              <v-text-field
+                v-model="title"
+                label="Title"
+                prepend-icon="mdi mdi-pen"
+              ></v-text-field>
+                <v-textarea
+                name="input-7-1"
+                label="Content"
+                prepend-icon="mdi mdi-text"
+                v-model="content"      
+                ></v-textarea>
+                <!-- Date Picker -->
+                <v-menu
+            >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              clearable
+              label="Due Date"
+              readonly
+              v-model="formattedDate"
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+          ></v-date-picker>
+        </v-menu>
+                <!-- Button -->
+                <v-btn class="success mt-2 mx-0" depressed small @click="submit()">Submit</v-btn>
+            </v-form>
         </v-card-text>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import format from 'date-fns/format'
 export default {
-
+data (){
+  return {
+    title:'',
+    content:'',
+    date:null
+  }
+},
+computed:{
+  formattedDate (){
+    return this.date? format(this.date, 'MMMM dd, yyyy') :''
+  }
+},
+methods:{
+  submit(){
+    console.log(this.title,this.content)
+  }
+},
 }
 </script>
 
